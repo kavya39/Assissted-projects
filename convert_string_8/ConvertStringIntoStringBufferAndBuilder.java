f260dc7cdcd7d1eff5a8ff7b0031ba31d0a3ea9d
kavya <kavyaroyal.bandi@gmail.com>
2022-03-07 09:56:20 +0530

    msg

---
+++ convert_string_8/ConvertStringIntoStringBufferAndBuilder.java
@@ -0,0 +1,20 @@
+package convert_string_8;
+
+public class ConvertStringIntoStringBufferAndBuilder {
+	 
+	    public static void main(String[] args) {
+	 
+	        String str1 = "This is Java Weblog. ";
+	        
+	        StringBuffer sb = new StringBuffer();
+	        sb.append(str1);
+
+	        System.out.println("Ex: String to StringBuffer using append() method : \n" + sb);
+	        
+	        System.out.println("******************* \n");
+	        
+	        StringBuilder sbl = new StringBuilder(str1);
+	        System.out.println("Ex: String to StringBuilder : \n" + sbl);
+
+	    }
+	}
---
+++ queueOperations_30/QueueProgram.java
@@ -0,0 +1,23 @@
+package queueOperations_30;
+import java.util.*;
+
+public class QueueProgram 
+{
+	public static void main(String[] args) 
+	{
+		Queue<String> locationsQueue = new LinkedList<>();
+		locationsQueue.add("Mango");
+	    locationsQueue.add("Apple");
+	    locationsQueue.add("Grapes");
+	    locationsQueue.add("Kiwi");
+	    locationsQueue.add("Orange");
+	    locationsQueue.add("Jackfruit");
+	    locationsQueue.add("Dragonfruit");
+
+	    System.out.println("Queue is : " + locationsQueue);
+	    System.out.println("Head of Queue : " + locationsQueue.peek());
+	    locationsQueue.remove();
+	    System.out.println("After removing Head of Queue : " + locationsQueue);
+	    System.out.println("Size of Queue : " + locationsQueue.size());
+	}
+}
